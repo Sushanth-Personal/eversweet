31e54f8c6718ede928f4ecb698fb3b408200cb54
Sushanth P <sushanthp.calicut@gmail.com>
2026-06-04 14:52:00 +0530

    Update: Clarity code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "3j9kkblzoj");`}
+          })(window, document, "clarity", "script", "x1p719fafw");`}
         </Script>
       </body>
     </html>
